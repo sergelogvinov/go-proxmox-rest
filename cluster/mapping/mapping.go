998b6d7794d66edb4c4303ee0649ec98bff1c4a2
Serge Logvinov <serge.logvinov@sinextra.dev>
2026-09-17 18:00:26 +0200

    docs: license

--- cluster/mapping/mapping.go
+++ cluster/mapping/mapping.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2026 Proxmox Community.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 // Package mapping provides access to the Proxmox VE cluster-wide hardware
 // mapping API (endpoints under /cluster/mapping): PCI, USB and directory
 // mappings that let a guest reference a logical device name instead of a
